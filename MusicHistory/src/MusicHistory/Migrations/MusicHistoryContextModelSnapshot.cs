dff501d002108f0fcaae8a91526731fdfab3e5f4
Paloma <paloma_vg@yahoo.com>
2016-06-10 12:43:37 -0500

    added first Migration

---
+++ MusicHistory/src/MusicHistory/Migrations/MusicHistoryContextModelSnapshot.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MusicHistory.Models;
+
+namespace MusicHistory.Migrations
+{
+    [DbContext(typeof(MusicHistoryContext))]
+    partial class MusicHistoryContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rc2-20901")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MusicHistory.Models.Album", b =>
+                {
+                    b.Property<int>("AlbumId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("ArtistId");
+
+                    b.Property<int>("GenreId");
+
+                    b.Property<string>("Title");
+
+                    b.Property<int>("Year");
+
+                    b.HasKey("AlbumId");
+
+                    b.ToTable("Album");
+                });
+
+            modelBuilder.Entity("MusicHistory.Models.Artist", b =>
+                {
+                    b.Property<int>("ArtistId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("ArtistId");
+
+                    b.ToTable("Artist");
+                });
+
+            modelBuilder.Entity("MusicHistory.Models.Genre", b =>
+                {
+                    b.Property<int>("GenreId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("GenreId");
+
+                    b.ToTable("Genre");
+                });
+
+            modelBuilder.Entity("MusicHistory.Models.Track", b =>
+                {
+                    b.Property<int>("TrackId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("AlbumId");
+
+                    b.Property<int>("ArtistId");
+
+                    b.Property<int>("GenreId");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("TrackId");
+
+                    b.ToTable("Track");
+                });
+
+            modelBuilder.Entity("MusicHistory.Models.User", b =>
+                {
+                    b.Property<int>("UserId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Password");
+
+                    b.Property<string>("UserName");
+
+                    b.HasKey("UserId");
+
+                    b.ToTable("User");
+                });
+        }
+    }
+}
